86a26e964ddd96bac4968a815462ccdca9a3fa22
pavan27v <109024676+pavan27v@users.noreply.github.com>
2023-11-14 10:09:31 +0000

    json

--- json.c
+++ json.c
@@ -10,7 +10,7 @@
 
 static const char *JSON_STRING =
     "{\"user\": \"johndoe\", \"admin\": false, \"uid\": 1000,\n  "
-    "\"groups\": [\"users\", \"wheel\", \"audio\", \"video\"]}";
+    "\"group\": [\"users\", \"wheel\", \"audio\", \"video\"]}";
 
 static int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
   if (tok->type == JSMN_STRING && (int)strlen(s) == tok->end - tok->start &&
